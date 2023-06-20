61456af23f49ec9592f4bd736f4d7dae0c08ce56
Joshua Daniel Harris <jdh335@DIGITAL-51.ee.byu.edu>
2023-06-20 14:24:45 -0600

    Lab9 Synth

--- lab9_project/main.c
+++ lab9_project/main.c
@@ -1,16 +1,27 @@
 #include "buttons.h"
+#include "switches.h"
 #include "sound.h"
 #include <stdio.h>
+#include <stdint.h>
 
 int main() {
   sound_init();
+  buttons_init();
+  switches_init();
+
+  uint8_t buttonsVal = buttons_read();
+  uint8_t switchesVal = switches_read();
+
+  printf("%d\n", buttonsVal);
+  printf("%d\n", switchesVal);
+
   sound_tick();
   sound_playSound(sound_c4_sine_e);
   
   while (1) {
     sound_tick();
     if (!sound_isBusy())
-      break;
+      sound_playSound(sound_c4_sine_e);
   }
   printf("done.\n");
 }
