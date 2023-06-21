796268788716740896cfa3a470a238a31eea8d94
Preston Thomas <prestondthomas72@gmail.com>
2023-06-20 18:00:23 -0600

    work in progress main.c

--- lab9_project/main.c
+++ lab9_project/main.c
@@ -9,6 +9,16 @@
 sound_sounds_t currSound() {}
 
 int main() {
+  sound_init();
+  switches_init();
+  sound_setVolume(sound_mediumHighVolume_e);
 
-
+  while (1) {
+    sound_tick();
+    uint8_t switchesVal = switches_read();
+    printf("%d\n", switchesVal);
+    if (!sound_isBusy()) {
+      sound_playSound(switchesVal);
+    }
+  }
 }
